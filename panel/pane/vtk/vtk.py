92f35a1e6515827f245e55575a6e649c0494b4f6
T.J. Corona <tj.corona@kitware.com>
2020-01-23 12:04:47 -0500

    remove change to dummy

--- panel/pane/vtk/vtk.py
+++ panel/pane/vtk/vtk.py
@@ -118,10 +118,8 @@ class VTK(PaneBase):
         return (None, None)
 
     def _update(self, model):
-#        model.scene, local_arrays = self._get_vtkjs()
-#        model.arrays.update(local_arrays)
         model.scene, model.arrays = self._get_vtkjs()
 
-        model.arrays_changed = not model.arrays_changed
+#        model.arrays_changed = not model.arrays_changed
         print('new arrays:', model.arrays.keys())
 
